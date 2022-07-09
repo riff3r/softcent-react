60e57c3fd8a98ee739717d7b195d1bea17f9b0df
riff3r <arif4alll@gmail.com>
2022-07-09 20:43:02 +0600

    image slider added

--- src/components/Layout/OurCustomer.js
+++ src/components/Layout/OurCustomer.js
@@ -1,10 +1,47 @@
-import React from "react";
+import Slider from "react-slick";
 import customerOne from "../Assets/images/customer/image-1.png";
 import customerTwo from "../Assets/images/customer/image-2.png";
 import customerThree from "../Assets/images/customer/image-3.png";
 import customerFour from "../Assets/images/customer/image-4.png";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const OurCustomer = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <section className="our-customer">
       <div className="container">
@@ -12,12 +49,52 @@ const OurCustomer = () => {
           <span>Our Customers</span>
         </h2>
 
-        <div className="customer-images flex">
-          <img src={customerOne} alt="UBS" />
-          <img src={customerTwo} alt="HSBC" />
-          <img src={customerThree} alt="Myfitlab" />
-          <img src={customerFour} alt="Appsero" />
-        </div>
+        <Slider {...settings}>
+          <div>
+            <h3>
+              <img src={customerOne} alt="UBS" />
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <img src={customerTwo} alt="HSBC" />
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <img src={customerThree} alt="Myfitlab" />
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <img src={customerFour} alt="Appsero" />
+            </h3>
+          </div>
+
+          <div>
+            <h3>
+              <img src={customerTwo} alt="HSBC" />
+            </h3>
+          </div>
+
+          <div>
+            <h3>
+              <img src={customerThree} alt="Myfitlab" />
+            </h3>
+          </div>
+
+          <div>
+            <h3>
+              <img src={customerOne} alt="UBS" />
+            </h3>
+          </div>
+
+          <div>
+            <h3>
+              <img src={customerFour} alt="Appsero" />
+            </h3>
+          </div>
+        </Slider>
       </div>
     </section>
   );
